b5a7cc7b028e10c80a4a3a4631e86f84bb0b62ac
KimSeWon <48078726+KimSeWon@users.noreply.github.com>
2019-05-26 21:24:06 +0900

    code change about simplechat
    +assignment of OSS class

--- ChatServer.java
+++ ChatServer.java
@@ -7,7 +7,7 @@ public class ChatServer {
 	public static void main(String[] args) {
 		try{
 			ServerSocket server = new ServerSocket(10001);
-			System.out.println("Waiting connection...");
+			System.out.println("Waiting connection....");
 			HashMap hm = new HashMap();
 			while(true){
 				Socket sock = server.accept();
@@ -45,11 +45,18 @@ class ChatThread extends Thread{
 	} // construcor
 	public void run(){
 		try{
+			String xx = "fuck shit damn silly fool";
 			String line = null;
 			while((line = br.readLine()) != null){
-				if(line.equals("/quit"))
+				String linee = line.toLowerCase();
+				if(linee.contains("fuck")||linee.contains("shit")||linee.contains("fool")||linee.contains("silly")||linee.contains("hell"))//5가지의 경우에서 문장이 포함 된 것이 있다면 쓰지말라고 경고를 준다. 대소문자 구분없이 만들었다.
+					dont();
+				else if(line.equals("/quit"))
 					break;
-				if(line.indexOf("/to ") == 0){
+				else if(line.equals("/userlist")){//userlist를 입력하면
+					send_userlist();//send_userlist메소드 실행
+				}
+				else if(line.indexOf("/to ") == 0){
 					sendmsg(line);
 				}else
 					broadcast(id + " : " + line);
@@ -85,11 +92,37 @@ class ChatThread extends Thread{
 		synchronized(hm){
 			Collection collection = hm.values();
 			Iterator iter = collection.iterator();
+			Iterator iter2 = hm.keySet().iterator();//새로운iter2를 만든다. 위의 iter과는 다르게 value값만이 아닌 키값도 같이 받아온다.
 			while(iter.hasNext()){
+				String keys = (String)iter2.next();//iter와 같은 순으로 넘긴다
 				PrintWriter pw = (PrintWriter)iter.next();
-				pw.println(msg);
+				if(id==keys){//현 아이디와 key값이 겹치면 보내지 않는 것으로 자신이 보낸 문장은 자신에게 나타나지 않는다.
+				}
+				else{
+					pw.println(msg);
+					pw.flush();
+				}
+			}
+		}
+	}// broadcast
+	public void send_userlist(){
+		synchronized(hm){
+			Object obj = hm.get(id);//아이디에 해당하는 value인 pw를 가져옴
+			if(obj != null){
+				PrintWriter pw = (PrintWriter)obj;
+				pw.println("Users id are "+ hm.size() +" : "+hm.keySet());//접속한 사용자들의 id들과 그 수를 출력
+				pw.flush();//해당 아이디에게만 보여짐
+			}
+		}
+	}
+	public void dont(){
+		synchronized(hm){
+			Object obj = hm.get(id);
+			if(obj != null){
+				PrintWriter pw = (PrintWriter)obj;
+				pw.println("don't use this!");
 				pw.flush();
 			}
 		}
-	} // broadcast
+	}
 }
